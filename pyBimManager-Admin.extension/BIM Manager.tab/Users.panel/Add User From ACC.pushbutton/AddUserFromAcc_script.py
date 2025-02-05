7511d5a377e91322188d0f18e9bf0b1298b590f3
jaredholloway94 <jaredholloway@outlook.com>
2025-02-05 09:34:19 -0600

    update error handling

--- pyBimManager-Admin.extension/BIM Manager.tab/Users.panel/Add User From ACC.pushbutton/AddUserFromAcc_script.py
+++ pyBimManager-Admin.extension/BIM Manager.tab/Users.panel/Add User From ACC.pushbutton/AddUserFromAcc_script.py
@@ -19,10 +19,30 @@ selection = SelectFromList.show(
     )
 
 if selection:
+
+    succeeded = []
+    failed = []
+
     # register user(s)
     for u in selection:
-        autodesk_id, name, email = u.split("  |  ")
-        register_user(autodesk_id, name, email, username='')
+        try:
+            autodesk_id, name, email = u.split("  |  ")
+            register_user(autodesk_id, name, email, username='')
+        except:
+            failed.append(u)
+        else:
+            succeeded.append(u)
+
+    if len(succeeded) > 0:
+        print("Successfully added hooks for these commands:\n")
+        for s in succeeded:
+            print("\t{}\n".format(s))
+
+    if len(failed) > 0:
+        print("Failed to add hooks for these commands:\n")
+        for f in failed:
+            print("\t{}\n".format(f))
+
 else:
     # user closed the selection window
     pass
--- pyBimManager-Admin.extension/BIM Manager.tab/Users.panel/Elevate User to Admin.pushbutton/ElevateUserToAdmin_script.py
+++ pyBimManager-Admin.extension/BIM Manager.tab/Users.panel/Elevate User to Admin.pushbutton/ElevateUserToAdmin_script.py
@@ -28,47 +28,55 @@ else:
         )
 
     if selection:
-        # update in-memory copy of ADMINS registry
+        
+        updates = 0
+
         for i in selection:
             autodesk_id, name, email, username = i.split('  |  ')
 
+            # check that we have a username registered...
             if username == '':
+                # ...if not, skip this user, and report the error...
                 print("ERROR: Cannot elevate the following user to Admin, because their username has not been registered:\n" + 
                       "       {}\n\n".format(i) +
                       "       Have the user open a new instance of Revit. This will register their username.\n" +
                       "       Then you can return here to elevate the user to Admin.\n\n" +
                       "       Any other users selected in the previous window will not be affected - they will still be elevated to Admin.\n"
                 )
+            else:
+                # ...else, update in-memory copy of ADMINS registry
+                ADMINS[autodesk_id] = {
+                    'name':name,
+                    'email':email,
+                    'username':username
+                }
+                updates += 1
 
+        # check if any changes were made to the ADMINS registry
+        if updates > 0:
+            # update stored copy of ADMINS registry
+            try:
+                with open(ADMINS_FILE,'w') as json_file:
+                    json.dump(ADMINS, json_file)
+            except:
+                raise Exception("Could not update admins.json.")
+            else:
+                print("Updated: {}".format(ADMINS_FILE))
 
-            ADMINS[autodesk_id] = {
-                'name':name,
-                'email':email,
-                'username':username
-            }
+            # update extensions.json (pyRevitCore and pyBimManager-Admin "authusers" fields)
+            for i,e in enumerate(EXTENSIONS['extensions']):
+                if 'authusers' in e:
+                    EXTENSIONS['extensions'][i]['authusers'] = [ ADMINS[a]['username'] for a in ADMINS ]
 
-        # update stored copy of ADMINS registry
-        try:
-            with open(ADMINS_FILE,'w') as json_file:
-                json.dump(ADMINS, json_file)
-        except:
-            raise Exception("Could not update admins.json.")
-        else:
-            print("Updated: {}".format(ADMINS_FILE))
+            try:
+                with open(EXTENSIONS_FILE,'w') as json_file:
+                    json.dump(EXTENSIONS, json_file)
+            except:
+                raise Exception("Could not update extensions.json.")
+            else:    
+                print("Updated: {}".format(EXTENSIONS_FILE))
 
-        # update extensions.json (pyRevitCore and pyBimManager-Admin "authusers" fields)
-        for i,e in enumerate(EXTENSIONS['extensions']):
-            if 'authusers' in e:
-                EXTENSIONS['extensions'][i]['authusers'] = [ ADMINS[a]['username'] for a in ADMINS ]
-
-        try:
-            with open(EXTENSIONS_FILE,'w') as json_file:
-                json.dump(EXTENSIONS, json_file)
-        except:
-            raise Exception("Could not update extensions.json.")
-        else:    
-            print("Updated: {}".format(EXTENSIONS_FILE))
-    
+        
     else:
         # user closed the window
         pass
