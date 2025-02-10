857eae0ea523db017b141468a09c71ea44a0c945
jaredholloway94 <jaredholloway@outlook.com>
2025-02-10 12:43:02 -0600

    update icons;

--- pyBimManager-Admin.extension/BIM Manager.tab/Config.Panel/Open Admins File.pushbutton/OpenAdminsFile_script.py
+++ pyBimManager-Admin.extension/BIM Manager.tab/Config.Panel/Open Admins File.pushbutton/OpenAdminsFile_script.py
@@ -4,5 +4,5 @@ from pyBimManager import ADMINS_FILE
 os.startfile(ADMINS_FILE)
 
 
-# lite icon: https://img.icons8.com/?size=100&id=86556&format=png&color=000000
-# dark icon: https://img.icons8.com/?size=100&id=86556&format=png&color=ffffff
+# lite icon: https://img.icons8.com/?size=100&id=89871&format=png&color=000000
+# dark icon: https://img.icons8.com/?size=100&id=89871&format=png&color=ffffff
--- pyBimManager-Admin.extension/BIM Manager.tab/Config.Panel/Open Users File.pushbutton/OpenUsersFile_script.py
+++ pyBimManager-Admin.extension/BIM Manager.tab/Config.Panel/Open Users File.pushbutton/OpenUsersFile_script.py
@@ -4,5 +4,5 @@ from pyBimManager import USERS_FILE
 os.startfile(USERS_FILE)
 
 
-# lite icon: https://img.icons8.com/?size=100&id=86556&format=png&color=000000
-# dark icon: https://img.icons8.com/?size=100&id=86556&format=png&color=ffffff
+# lite icon: https://img.icons8.com/?size=100&id=89871&format=png&color=000000
+# dark icon: https://img.icons8.com/?size=100&id=89871&format=png&color=ffffff
