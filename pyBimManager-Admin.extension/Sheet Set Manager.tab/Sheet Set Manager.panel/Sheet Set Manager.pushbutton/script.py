4c12823352f47d5ba2d0aaaeb92da8c30836ef1e
Jared Holloway <41764450+jaredholloway94@users.noreply.github.com>
2025-06-09 11:57:00 -0500

    added icon;

--- pyBimManager-Admin.extension/Sheet Set Manager.tab/Sheet Set Manager.panel/Sheet Set Manager.pushbutton/script.py
+++ pyBimManager-Admin.extension/Sheet Set Manager.tab/Sheet Set Manager.panel/Sheet Set Manager.pushbutton/script.py
@@ -15,3 +15,6 @@ if __name__ == "__main__":
     # handle case when the user closes the window via the 'X' button
     if transaction_group.HasStarted():
         transaction_group.RollBack()
+
+
+# icon: https://icons8.com/icon/LoJeUoSeKwC0/project-setup
